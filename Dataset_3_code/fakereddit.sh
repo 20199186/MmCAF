dcc667cc29c45fb54e73be21749e8184f50a0e76
20199186 <109344488+20199186@users.noreply.github.com>
2022-07-16 15:06:21 +0530

    Add files via upload

---
+++ Dataset_3_code/fakereddit.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH -N 1 #1 Node
+#SBATCH --ntasks-per-node=8
+#SBATCH --time=3-00:00:00
+#SBATCH --job-name=run_models_gpu_1_16GB
+#SBATCH --error=%J.err
+#SBATCH --output=%J.out
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:1
+
+
+conda activate test_multimod
+
+~/.conda/envs/test_multimod/bin/python /scratch/satyendrac.mnitjaipur/codes/monika/fakereddit/fakereddit_usc_efficientnet_average_test.py
+
